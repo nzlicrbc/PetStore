0d4d31e9f025ed611c1b8ce607bee3017d713291
nzlicrbc <nzlcn10@hotmail.com>
2026-06-21 01:37:56 +0300

    feat: add featured products section

--- main.js
+++ main.js
@@ -65,3 +65,8 @@ ScrollReveal().reveal(".about__content p" , {
     ...scrollRevealOption,
     delay: 1500,
 });
+
+ScrollReveal().reveal(".product__card" , {
+    ...scrollRevealOption,
+    interval: 500,
+});
