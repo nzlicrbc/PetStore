edb244ba1577995d1404e3c13aa5c38ed30e3bfc
nzlicrbc <nzlcn10@hotmail.com>
2026-06-21 00:27:22 +0300

    feat: add scroll reveal animations and responsive header fixes

--- main.js
+++ main.js
@@ -39,3 +39,8 @@ ScrollReveal().reveal(".header__btn" , {
     ...scrollRevealOption,
     delay: 2000,
 });
+
+ScrollReveal().reveal(".intro__card" , {
+    ...scrollRevealOption,
+    interval: 500,
+});
